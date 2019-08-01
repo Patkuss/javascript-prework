9f27c329fdffcdca9844e6e5e896f21a9dcae461
Patryk Płaczek <patryk.placzek@wp.pl>
2019-08-01 19:55:55 +0200

    Add first logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,35 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
+// computer move
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałeś ' + playerMove + '! Mój ruch to ' + computerMove + ', wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+    computerMove = 'kamień';
+}   else if (randomNumber == 2) {
+    computerMove = 'papier';
+}   else if (randomNumber == 3) {
+    computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
+// player move
+
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}   else if (randomNumber == 2) {
+    playerMove = 'papier';
+}   else if (randomNumber == 3) {
+    playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
